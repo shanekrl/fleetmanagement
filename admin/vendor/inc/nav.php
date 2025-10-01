9dfc78bfa08a5db862b4e9899f7ded581eab0350
JovitaTipon <tiponjovita@gmail.com>
2025-10-01 21:50:55 +0800

    admin navbar logout fix

--- admin/vendor/inc/nav.php
+++ admin/vendor/inc/nav.php
@@ -18,7 +18,7 @@
         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
           <a class="dropdown-item" href="admin-profile.php">Profile</a>
           <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">Logout</a>
+          <a class="dropdown-item" href="admin-logout.php">Logout</a>
         </div>
       </li>
     </ul>
