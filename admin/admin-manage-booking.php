0da047a6d5a6fdcc9eb86a95d25ebb4c56c6ec39
JovitaTipon <tiponjovita@gmail.com>
2025-10-11 15:32:12 +0800

    stray = in admin cancelled trips

--- admin/admin-manage-booking.php
+++ admin/admin-manage-booking.php
@@ -98,7 +98,6 @@ if (table_exists($mysqli,'v_booking_grid')) {
     border-color:#000047 !important; color:#000047 !important; background:#fff !important;
     box-shadow: inset 0 -2px 0 #000047;
   }
-  =
   .btn-group .btn.kaya-tab:not(.active){ border-color:#d9deee !important; background:#fff !important; }
   .btn-group .btn.kaya-tab:not(.active):hover{ border-color:#b9c2dd !important; background:#f6f8ff !important; }
 </style>
